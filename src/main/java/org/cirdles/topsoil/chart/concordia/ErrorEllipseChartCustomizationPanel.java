453bf1f22211b071eb4ffadf986c86bbdeafb114
Florent <florent.pastor@gmail.com>
2014-05-23 13:47:36 -0400

    Moved ChartCustomizationPanel out of ErrorEllipseChartCustomizationPanel

--- src/main/java/org/cirdles/topsoil/chart/concordia/ErrorEllipseChartCustomizationPanel.java
+++ src/main/java/org/cirdles/topsoil/chart/concordia/ErrorEllipseChartCustomizationPanel.java
@@ -15,24 +15,12 @@
  */
 package org.cirdles.topsoil.chart.concordia;
 
-import java.io.IOException;
-import java.util.ResourceBundle;
-import javafx.beans.binding.Bindings;
-import javafx.beans.value.ObservableValue;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
 import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import org.cirdles.jfxutils.NumberField;
 import org.cirdles.topsoil.Tools;
-import org.cirdles.topsoil.chart.NumberAxis;
+import org.cirdles.topsoil.chart.concordia.panels.ChartCustomizationPanel;
 import org.cirdles.topsoil.chart.concordia.panels.ErrorEllipsesCustomisationPanel;
 
 /**
@@ -62,70 +50,6 @@ public class ErrorEllipseChartCustomizationPanel extends VBox {
         getChildren().add(title);
         getChildren().add(eeCustomizationPane);
         getChildren().add(ccCustomizationPane);
-    }
-
-    private static class ChartCustomizationPanel extends VBox {
-
-        @FXML
-        CheckBox checkboxConcordia;
-        @FXML
-        Label anchorTickLabel;
-        @FXML
-        Label tickUnitLabel;
-        @FXML
-        NumberField tickXnf;
-        @FXML
-        NumberField tickUnitXnf;
-        @FXML
-        CheckBox autoTickXCheckBox;
-        @FXML
-        NumberField tickYnf;
-        @FXML
-        NumberField tickUnitYnf;
-        @FXML
-        CheckBox autoTickYCheckBox;
-
-        public ChartCustomizationPanel(ErrorEllipseChart chart) {
-            super(5);
-
-            NumberAxis xAxis = (NumberAxis) chart.getXAxis();
-            NumberAxis yAxis = (NumberAxis) chart.getYAxis();
-
-            FXMLLoader loader = new FXMLLoader(ChartCustomizationPanel.class.getResource("chartcustomizationpanel.fxml"),
-                                               ResourceBundle.getBundle("org.cirdles.topsoil.Resources"));
-            loader.setRoot(this);
-            loader.setController(this);
-
-            try {
-                loader.load();
-                chart.concordiaLineShownProperty().bind(checkboxConcordia.selectedProperty());
-
-                ObservableValue<Number> xRange = xAxis.upperBoundProperty().subtract(xAxis.lowerBoundProperty());
-                ObservableValue<Number> yRange = yAxis.upperBoundProperty().subtract(yAxis.lowerBoundProperty());
-
-                autoTickXCheckBox.selectedProperty().bindBidirectional(((NumberAxis) chart.getXAxis()).getTickGenerator().autoTickingProperty());
-
-                autoTickYCheckBox.selectedProperty().bindBidirectional(((NumberAxis) chart.getYAxis()).getTickGenerator().autoTickingProperty());
-
-                tickXnf.setTargetProperty(((NumberAxis) chart.getXAxis()).getTickGenerator().anchorTickProperty(), xRange);
-                tickXnf.visibleProperty().bind(Bindings.not(autoTickXCheckBox.selectedProperty()));
-                tickYnf.setTargetProperty(((NumberAxis) chart.getYAxis()).getTickGenerator().anchorTickProperty(), yRange);
-                tickYnf.visibleProperty().bind(Bindings.not(autoTickYCheckBox.selectedProperty()));
-
-                tickUnitXnf.setTargetProperty(((NumberAxis) chart.getXAxis()).getTickGenerator().tickUnitProperty(), xRange);
-                tickUnitXnf.visibleProperty().bind(Bindings.not(autoTickXCheckBox.selectedProperty()));
-                tickUnitYnf.setTargetProperty(((NumberAxis) chart.getYAxis()).getTickGenerator().tickUnitProperty(), yRange);
-                tickUnitYnf.visibleProperty().bind(Bindings.not(autoTickYCheckBox.selectedProperty()));
-
-                tickUnitLabel.visibleProperty().bind(Bindings.and(autoTickXCheckBox.selectedProperty(), autoTickYCheckBox.selectedProperty()).not());
-
-                anchorTickLabel.visibleProperty().bind(Bindings.and(autoTickXCheckBox.selectedProperty(), autoTickYCheckBox.selectedProperty()).not());
-            } catch (IOException e) {
-                getChildren().add(new Label("There was an error loading this part of the panel."));
-                e.printStackTrace();
-            }
-
-        }
     }
 
 }
