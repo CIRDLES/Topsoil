1751c5a38fd044acbd4f52223c6d24a382ea75a0
John Zeringue <zeringus@gmail.com>
2014-05-30 13:51:55 -0400

    Added documentation to vector classes

--- src/main/java/org/cirdles/math/Vector2D.java
+++ src/main/java/org/cirdles/math/Vector2D.java
@@ -16,37 +16,79 @@
 package org.cirdles.math;
 
 /**
- *
- * @author zeringuej
+ * A two-dimensional vector.
  */
 public class Vector2D extends Vector3D {
 
+    /**
+     * Creates a new two-dimensional vector with the specified x- and y- components.
+     *
+     * @param x the x-component
+     * @param y the y-component
+     */
     public Vector2D(double x, double y) {
         super(x, y, 0);
     }
 
+    /**
+     * Returns a new vector representing the result of adding this vector to the other vector passed as the argument.
+     * The vectors are added by creating a new vector where each component is the result of adding the corresponding
+     * components of the operands.
+     *
+     * @param vector the other vector
+     * @return the sum of this vector and the other
+     */
     public Vector2D plus(Vector2D vector) {
         return new Vector2D(x() + vector.x(),
                             y() + vector.y());
     }
 
+    /**
+     * Returns a new vector representing the result of subtracting the specified vector from this one. The vectors are
+     * subtracted by creating a new vector where each component is the result of subtracting the corresponding
+     * components of the operands.
+     *
+     * @param vector the other vector
+     * @return this vector minus the other
+     */
     public Vector2D minus(Vector2D vector) {
         return new Vector2D(x() - vector.x(),
                             y() - vector.y());
     }
 
+    /**
+     * {@inheritDoc}
+     * @return 
+     */
     @Override
     public Vector2D times(double scalar) {
         return new Vector2D(scalar * x(),
                             scalar * y());
     }
 
+    /**
+     * {@inheritDoc}
+     * @return 
+     */
     @Override
     public Vector2D dividedBy(double scalar) {
         return new Vector2D(x() / scalar,
                             y() / scalar);
     }
 
+    /**
+     * {@inheritDoc}
+     * @return 
+     */
+    @Override
+    public Vector2D normalized() {
+        return this.dividedBy(this.norm());
+    }
+
+    /**
+     * {@inheritDoc}
+     * @return 
+     */
     @Override
     public String toString() {
         return String.format("(%.5f, %.5f)", x(), y());
