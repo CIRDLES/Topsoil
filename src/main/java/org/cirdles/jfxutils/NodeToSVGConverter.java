0b1dea568bc3c9941905813279d32d21f20b22fe
John Zeringue <zeringus@gmail.com>
2014-06-05 11:31:15 -0400

    Issue #52 fix
    Fixes #52 (at least I think) by not inserting invisible paths in the
    SVG. I’ll test more tomorrow or tonight to insure that this issue is
    properly resolved.

--- src/main/java/org/cirdles/jfxutils/NodeToSVGConverter.java
+++ src/main/java/org/cirdles/jfxutils/NodeToSVGConverter.java
@@ -219,6 +219,10 @@ public class NodeToSVGConverter {
             element.setAttribute("stroke-width", String.valueOf(line.getStrokeWidth()));
         } else if (node instanceof Path) {
             Path path = (Path) node;
+            
+            if (colorToRGBString((Color) path.getStroke()).equals("none")) {
+                return null;
+            }
 
             element = document.createElement("path");
 
