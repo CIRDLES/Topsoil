05e7e794f5d36a98a9bec8de8138f077ff4aac58
John Zeringue <john.joseph.zeringue@gmail.com>
2014-10-13 11:45:51 -0400

    Fixed "Export to SVG" to avoid JVM bug.

--- src/main/java/org/cirdles/topsoil/chart/concordia/ErrorChartToolBar.java
+++ src/main/java/org/cirdles/topsoil/chart/concordia/ErrorChartToolBar.java
@@ -28,18 +28,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ToolBar;
 import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 import javafx.util.StringConverter;
 import org.cirdles.jfxutils.NodeToSVGConverter;
-import org.cirdles.jfxutils.NumberField;
 import org.cirdles.topsoil.builder.TopsoilBuilderFactory;
-import org.cirdles.topsoil.chart.concordia.panels.SVGExportDialog;
 import org.controlsfx.control.MasterDetailPane;
 
 /**
@@ -105,9 +101,17 @@ public class ErrorChartToolBar extends ToolBar {
 
     @FXML
     private void exportToSVG() {
-        //start_turnNodeToText(chart); (Tool for developper/ should always be commented before commit)
-        SVGExportDialog exportpanel = new SVGExportDialog(this, chart);
-        exportpanel.show();
+        NodeToSVGConverter converter = new NodeToSVGConverter();
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Export to SVG");
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("SVG Image", "*.svg"));
+        File file = fileChooser.showSaveDialog(getScene().getWindow());
+
+        if (file != null) {
+            converter.convert(chart, file);
+        }
     }
 
     @FXML
