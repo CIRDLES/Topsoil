ab032b3455001129be4a9a50c98d2c62ec633943
John Zeringue <john.joseph.zeringue@gmail.com>
2015-09-29 23:12:02 -0400

    Moved standard charts into core
    Moved standard charts (error ellipse and scatterplot) into `core`, so
    that they may be used without including the `app` module.

--- core/config/buster.js
+++ core/config/buster.js
@@ -4,7 +4,8 @@ config["topsoil.js"] = {
     environment: "browser",
     rootPath: "../",
     sources: [
-        "src/main/resources/org/cirdles/topsoil/chart/**/*.js"
+        "src/main/resources/org/cirdles/topsoil/chart/*.js",
+        "src/main/resources/org/cirdles/topsoil/chart/topsoil_js/*.js"
     ],
     tests: [
         "src/test/resources/org/cirdles/topsoil/chart/*Test.js"
