b16ac1632cd9525e1e836e0a218f4aeb674a5139
John Zeringue <john.joseph.zeringue@gmail.com>
2016-01-30 17:45:47 -0500

    Removed `VariableContext#getVariables`
    Removed `VariableContext#getVariables` as it's unused and doesn't fit
    with future ideas for `VariableContext`.

--- core/src/main/java/org/cirdles/topsoil/plot/VariableContext.java
+++ core/src/main/java/org/cirdles/topsoil/plot/VariableContext.java
@@ -15,12 +15,13 @@
  */
 package org.cirdles.topsoil.plot;
 
-import java.util.Collection;
-import java.util.Objects;
-import java.util.Optional;
 import org.cirdles.topsoil.dataset.Dataset;
 import org.cirdles.topsoil.dataset.entry.Entry;
 import org.cirdles.topsoil.dataset.field.Field;
+
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Optional;
 
 /**
  *
@@ -28,13 +29,11 @@ import org.cirdles.topsoil.dataset.field.Field;
  */
 public interface VariableContext {
 
-    public Collection<Variable> getVariables();
+    Dataset getDataset();
 
-    public Dataset getDataset();
+    Collection<VariableBinding> getBindings();
 
-    public Collection<VariableBinding> getBindings();
-
-    public default <T> Optional<VariableBinding<T>> getBindingForVariable(Variable<T> variable) {
+    default <T> Optional<VariableBinding<T>> getBindingForVariable(Variable<T> variable) {
         return getBindings().stream()
                 .filter(binding -> {
                     return Objects.equals(binding.getVariable(), variable);
@@ -44,16 +43,16 @@ public interface VariableContext {
                 .map(binding -> (VariableBinding<T>) binding);
     }
 
-    public default <T> void addBinding(Variable<T> variable, Field<T> field) {
+    default <T> void addBinding(Variable<T> variable, Field<T> field) {
         addBinding(variable, field, variable.getFormats().get(0));
     }
 
-    public default <T> Optional<T> getValue(Variable<T> variable, Entry entry) {
+    default <T> Optional<T> getValue(Variable<T> variable, Entry entry) {
         return getBindingForVariable(variable).map(binding -> {
             return binding.getFormat().normalize(binding, entry);
         });
     }
 
-    public <T> void addBinding(Variable<T> variable, Field<T> field, VariableFormat<T> format);
+    <T> void addBinding(Variable<T> variable, Field<T> field, VariableFormat<T> format);
 
 }
