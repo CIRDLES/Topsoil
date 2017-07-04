908845102d58573a90c5aa08596db9462c03cd51
Drew Bigelow <rdbigelow@gmail.com>
2017-07-04 09:41:43 -0400

    Changed crosses to unct. bars

--- core/src/main/resources/org/cirdles/topsoil/plot/base/data/UncertaintyBars.js
+++ core/src/main/resources/org/cirdles/topsoil/plot/base/data/UncertaintyBars.js
@@ -2,42 +2,42 @@
  * @author Jake Marotta
  */
 
-if (plot.crossesVisible == null) {
-    plot.crossesVisible = false;
+if (plot.uncertaintyBarsVisible == null) {
+    plot.uncertaintyBarsVisible = false;
 }
 
-plot.drawCrosses = function () {
+plot.drawUncertaintyBars = function () {
 
-    // Removes crosses before redrawing them.
-    if (plot.crossesVisible) {
-        plot.removeCrosses();
+    // Removes Uncertainty Bars before redrawing them.
+    if (plot.uncertaintyBarsVisible) {
+        plot.removeUncertaintyBars();
     }
 
-    // Creates a separate SVG group for cross elements.
+    // Creates a separate SVG group for Uncertainty Bar elements.
     if (plot.pointGroup == null) {
-        plot.crossGroup = plot.dataGroup.append("g")
-            .attr("class", "crossGroup");
+        plot.uncertaintyBarGroup = plot.dataGroup.append("g")
+            .attr("class", "uncertaintyBarGroup");
     } else {
-        plot.crossGroup = plot.dataGroup.insert("g", ".pointGroup")
-            .attr("class", "crossGroup");
+        plot.uncertaintyBarGroup = plot.dataGroup.insert("g", ".pointGroup")
+            .attr("class", "uncertaintyBarGroup");
     }
 
-    plot.crossesVisible = true;
-    plot.updateCrosses();
+    plot.uncertaintyBarsVisible = true;
+    plot.updateUncertaintyBars();
 
 };
 
-plot.updateCrosses = function () {
-    if (plot.crossesVisible) {
+plot.updateUncertaintyBars = function () {
+    if (plot.uncertaintyBarsVisible) {
 
-        var crosses = plot.crossGroup.selectAll(".cross").data(plot.data);
+        var uncertaintyBars = plot.ncertaintyBarGroup.selectAll(".uncertaintyBar").data(plot.data);
 
-        crosses.exit().remove();
+        uncertaintyBars.exit().remove();
 
-        var cEnter = crosses.enter().append("g").data(plot.data)
-            .attr("class", "cross");
+        var cEnter = uncertaintyBars.enter().append("g").data(plot.data)
+            .attr("class", "uncertaintyBar");
 
-        // Adds the parts of the cross
+        // Adds the parts of the Uncertainty Bar
         cEnter.append("line").attr("class", "HLine");
         cEnter.append("line").attr("class", "VLine");
         cEnter.append("line").attr("class", "topCap");
@@ -50,13 +50,13 @@ plot.updateCrosses = function () {
         // of 1px. This doesn't completely solve the problem, and there is a slight jump in line appearance between 100%
         // opacity and 99% opacity, but overall, zooming looks much, much smoother.
         var strokeWidth;
-        if (plot.getProperty("Cross Opacity") < 1.0) {
+        if (plot.getProperty("Uncertainty Bar Opacity") < 1.0) {
             strokeWidth = 2;
         } else {
             strokeWidth = 1;
         }
 
-        crosses.select(".HLine")
+        uncertaintyBars.select(".HLine")
             .attr("y1", function (d) {
                 return plot.yAxisScale(d.y);
             })
@@ -69,11 +69,11 @@ plot.updateCrosses = function () {
             .attr("x2", function (d) {
                 return plot.xAxisScale(d.x + (plot.uncertainty * d.sigma_x));
             })
-            .attr("opacity", plot.getProperty("Cross Opacity"))
+            .attr("opacity", plot.getProperty("Uncertainty Bar Opacity"))
             .attr("stroke-width", strokeWidth)
-            .attr("stroke", plot.getProperty("Cross Fill Color"));
+            .attr("stroke", plot.getProperty("Uncertainty Bar Fill Color"));
 
-        crosses.select(".VLine")
+        uncertaintyBars.select(".VLine")
             .attr("y1", function (d) {
                 return plot.yAxisScale(d.y - (plot.uncertainty * d.sigma_y));
             })
@@ -86,11 +86,11 @@ plot.updateCrosses = function () {
             .attr("x2", function (d) {
                 return plot.xAxisScale(d.x);
             })
-            .attr("opacity", plot.getProperty("Cross Opacity"))
+            .attr("opacity", plot.getProperty("Uncertainty Bar Opacity"))
             .attr("stroke-width", strokeWidth)
-            .attr("stroke", plot.getProperty("Cross Fill Color"));
+            .attr("stroke", plot.getProperty("Uncertainty Bar Fill Color"));
 
-        crosses.select(".topCap")
+        uncertaintyBars.select(".topCap")
             .attr("y1", function (d) {
                 return plot.yAxisScale(d.y + (plot.uncertainty * d.sigma_y));
             })
@@ -103,11 +103,11 @@ plot.updateCrosses = function () {
             .attr("x2", function (d) {
                 return plot.xAxisScale(d.x + 0.2*(plot.uncertainty * d.sigma_x));
             })
-            .attr("opacity", plot.getProperty("Cross Opacity"))
+            .attr("opacity", plot.getProperty("Uncertainty Bar Opacity"))
             .attr("stroke-width", strokeWidth)
-            .attr("stroke", plot.getProperty("Cross Fill Color"));
+            .attr("stroke", plot.getProperty("Uncertainty Bar Fill Color"));
 
-        crosses.select(".leftCap")
+        uncertaintyBars.select(".leftCap")
             .attr("y1", function (d) {
                 return plot.yAxisScale(d.y - 0.2*(plot.uncertainty * d.sigma_y));
             })
@@ -120,11 +120,11 @@ plot.updateCrosses = function () {
             .attr("x2", function (d) {
                 return plot.xAxisScale(d.x - (plot.uncertainty * d.sigma_x));
             })
-            .attr("opacity", plot.getProperty("Cross Opacity"))
+            .attr("opacity", plot.getProperty("Uncertainty Bar Opacity"))
             .attr("stroke-width", strokeWidth)
-            .attr("stroke", plot.getProperty("Cross Fill Color"));
+            .attr("stroke", plot.getProperty("Uncertainty Bar Fill Color"));
 
-        crosses.select(".bottomCap")
+        uncertaintyBars.select(".bottomCap")
             .attr("y1", function (d) {
                 return plot.yAxisScale(d.y - (plot.uncertainty * d.sigma_y));
             })
@@ -137,11 +137,11 @@ plot.updateCrosses = function () {
             .attr("x2", function (d) {
                 return plot.xAxisScale(d.x + 0.2*(plot.uncertainty * d.sigma_x));
             })
-            .attr("opacity", plot.getProperty("Cross Opacity"))
+            .attr("opacity", plot.getProperty("Uncertainty Bar Opacity"))
             .attr("stroke-width", strokeWidth)
-            .attr("stroke", plot.getProperty("Cross Fill Color"));
+            .attr("stroke", plot.getProperty("Uncertainty Bar Fill Color"));
 
-        crosses.select(".rightCap")
+        uncertaintyBars.select(".rightCap")
             .attr("y1", function (d) {
                 return plot.yAxisScale(d.y - 0.2*(plot.uncertainty * d.sigma_y));
             })
@@ -154,15 +154,15 @@ plot.updateCrosses = function () {
             .attr("x2", function (d) {
                 return plot.xAxisScale(d.x + (plot.uncertainty * d.sigma_x));
             })
-            .attr("opacity", plot.getProperty("Cross Opacity"))
+            .attr("opacity", plot.getProperty("Uncertainty Bar Opacity"))
             .attr("stroke-width", strokeWidth)
-            .attr("stroke", plot.getProperty("Cross Fill Color"));
+            .attr("stroke", plot.getProperty("Uncertainty Bar Fill Color"));
     }
 };
 
-plot.removeCrosses = function () {
-    if (plot.crossesVisible) {
-        plot.crossGroup.remove();
-        plot.crossesVisible = false;
+plot.removeUncertaintyBars = function () {
+    if (plot.uncertaintyBarsVisible) {
+        plot.uncertaintyBarGroup.remove();
+        plot.uncertaintyBarsVisible = false;
     }
 };
