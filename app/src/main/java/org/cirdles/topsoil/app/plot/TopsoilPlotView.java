2a7c7f02a918b43158e19280ac0720b09f5e9f94
BryceBarrett <brycebarrett01@gmail.com>
2019-01-11 11:14:27 -0500

    Allows Plot to be Exported to a PDF (#431)
    * FXML
    
    * Export to PDF functionality
    
    * Removed unwanted API's
    
    * User can choose location of file
    
    * Preserves uncertainty format in SVG and PDF
    
    * No longer stores temp. image on user's system

--- app/src/main/java/org/cirdles/topsoil/app/plot/TopsoilPlotView.java
+++ app/src/main/java/org/cirdles/topsoil/app/plot/TopsoilPlotView.java
@@ -2,22 +2,33 @@ package org.cirdles.topsoil.app.plot;
 
 import javafx.application.Platform;
 import javafx.concurrent.Worker;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Button;
+import javafx.scene.control.SplitPane;
 import javafx.scene.control.ToolBar;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import org.cirdles.commons.util.ResourceExtractor;
+import org.cirdles.topsoil.app.uncertainty.UncertaintyFormat;
 import org.cirdles.topsoil.plot.DefaultProperties;
 import org.cirdles.topsoil.app.plot.panel.PlotPropertiesPanel;
 import org.cirdles.topsoil.plot.JavaScriptPlot;
 import org.cirdles.topsoil.plot.Plot;
 import org.cirdles.topsoil.plot.PlotProperty;
+import org.cirdles.topsoil.plot.internal.PDFSaver;
 import org.cirdles.topsoil.plot.internal.SVGSaver;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
+import javax.imageio.ImageIO;
+import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
@@ -33,6 +44,7 @@ public class TopsoilPlotView extends VBox {
 
 	@FXML private ToolBar toolbar;
 	@FXML private Button saveSVGButton;
+	@FXML private Button savePDFButton;
 	@FXML private Button resetViewButton;
 	@FXML private Button snapToCornersButton;
 
@@ -83,7 +95,29 @@ public class TopsoilPlotView extends VBox {
 	//**********************************************//
 
 	@FXML private void saveSVGButtonAction() {
-		new SVGSaver().save(plot.displayAsSVGDocument());
+
+		String uncValue = propertiesPanel.getUncertaintyFormat().toString();
+		Document doc = plot.displayAsSVGDocument();
+
+		Element ele = doc.getDocumentElement();
+		Element textNode = doc.createElement("text");
+		textNode.setAttribute("x", "230");
+		textNode.setAttribute("y", "553");
+		textNode.setAttribute("font-family", "sans-serif");
+		textNode.setAttribute("font-size", "18px");
+		textNode.setTextContent("Uncertainty Format: " + uncValue);
+
+		ele.appendChild(textNode);
+
+		new SVGSaver().save(doc);
+	}
+
+	@FXML private void savePDFButtonAction() {
+
+		WritableImage plotSnap = plotAnchorPane.snapshot(new SnapshotParameters(), null);
+		String uncValue = propertiesPanel.getUncertaintyFormat().toString();
+		PDFSaver.saveToPDF(plotSnap, uncValue);
+
 	}
 
 	@FXML private void resetViewButtonAction() {
