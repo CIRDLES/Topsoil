1bc845d2f8bfdb8235bf7da38dd9188db662825d
Garrett Brenner <garrebrenna@gmail.com>
2020-04-13 09:59:26 -0700

    added PlotStyleSerializer.java

---
+++ topsoilApp/src/main/java/org/cirdles/topsoil/app/file/serialization/PlotStyleSerializer.java
@@ -0,0 +1,10 @@
+package org.cirdles.topsoil.app.file.serialization;
+
+/**
+ * A class for serializing SimpleSymbolMap objects in order to save the state of a Plot.
+ *
+ * @author Garrett Brenner
+ */
+public class PlotStyleSerializer {
+
+}
